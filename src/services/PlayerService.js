8d742d2041bead5d87ce09dcaa8194a42fa8ad0b
Eric Garcia <eric.garcia.it@gmail.com>
2021-09-06 12:05:39 -0500

    Se calcula el alcance por equipos

--- src/services/PlayerService.js
+++ src/services/PlayerService.js
@@ -23,11 +23,53 @@ const getPlayerPerformance = player => {
     return {...player}
 }
 
+const filterByMeta = element => {
+    return element.meta !== undefined && element.meta !== null
+}
+
+const getFirstTeamPerfomance = player => {
+    return {
+        equipo: player.equipo,
+        goles: player.goles,
+        meta: player.meta,
+        alcance: (player.goles/player.meta) * 100
+    }
+}
+
+const getSuccessiveTeamPerformance = (player, teamPerformance) => {
+    const goles = teamPerformance.goles + player.goles
+    const meta = teamPerformance.meta + player.meta
+    return {
+        goles,
+        meta,
+        alcance: (goles/meta) * 100,
+        equipo: teamPerformance.equipo
+    }
+}
+
+const getTeamsPerformanceByPlayer = (teamsPerformances, player) => {
+    const teamPerformance = teamsPerformances[player.equipo]
+    if(teamPerformance){
+        teamsPerformances[player.equipo] = getSuccessiveTeamPerformance(player, teamPerformance)
+    }else{
+        teamsPerformances[player.equipo] = getFirstTeamPerfomance(player)
+    }
+    return teamsPerformances
+}
+
+const getTeamsPerformance = players => {
+    return players.filter(filterByMeta)
+    .reduce( getTeamsPerformanceByPlayer,{})
+}
+
 const getSalaries = players => {
     const playersWithPerformance = players
         .map(player => getPlayerObjective(player, TEAMS_EJEMPLO_RESUELVE_BD))
         .map(getPlayerPerformance)
+    const teamsPerformance = getTeamsPerformance(playersWithPerformance)
     console.log(playersWithPerformance)
+    console.log(teamsPerformance)
+
 }
 
 module.exports = {

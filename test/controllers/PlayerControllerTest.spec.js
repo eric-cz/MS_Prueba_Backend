396bf984f4e25c2f091be9e8f362b7891f35aaba
Eric Garcia <eric.garcia.it@gmail.com>
2021-09-06 15:05:21 -0500

    Se agregan pruebas unitarias de error en la estructura del json

--- test/controllers/PlayerControllerTest.spec.js
+++ test/controllers/PlayerControllerTest.spec.js
@@ -9,7 +9,6 @@ const { EJEMPLO_RESUELVE_FC, EJEMPLO_ROJO_AZUL } = require('../data/Players')
 
 
 describe('Testing POST /players/salaries',()=>{
-
    it('1. Calcular salario El Rulo', (done) => {
     chai.request(app).post(`${CONTEXT_APP}/players/salaries`)
     .send(EJEMPLO_ROJO_AZUL)
@@ -32,6 +31,47 @@ describe('Testing POST /players/salaries',()=>{
         expect(res.body.jugadores).to.have.lengthOf(EJEMPLO_RESUELVE_FC.jugadores.length)
         const luis = res.body.jugadores.find( x => x.nombre === 'Luis')
         expect(luis.sueldo_completo).to.equal(59550)
+        done()
+     })
+   })
+
+   it('3. Error de estructura', (done) => {
+    chai.request(app).post(`${CONTEXT_APP}/players/salaries`)
+    .send({})
+    .end( function(err,res){
+        expect(res).to.have.status(400);
+        expect(res.body).to.have.property('mensaje', 'La solicitud no cumple con una estrutura correcta')
+        expect(res.body).to.have.property('errores')
+     })
+     chai.request(app).post(`${CONTEXT_APP}/players/salaries`)
+     .send({jugadores : {}})
+     .end( function(err,res){
+         expect(res).to.have.status(400);
+         expect(res.body).to.have.property('mensaje', 'La solicitud no cumple con una estrutura correcta')
+         expect(res.body).to.have.property('errores')
+      })
+      chai.request(app).post(`${CONTEXT_APP}/players/salaries`)
+     .send({jugadores : []})
+     .end( function(err,res){
+         expect(res).to.have.status(400);
+         expect(res.body).to.have.property('mensaje', 'La solicitud no cumple con una estrutura correcta')
+         expect(res.body).to.have.property('errores')
+
+      })
+      chai.request(app).post(`${CONTEXT_APP}/players/salaries`)
+      .send({jugadores : [{}]})
+      .end( function(err,res){
+          expect(res).to.have.status(400);
+          expect(res.body).to.have.property('mensaje', 'La solicitud no cumple con una estrutura correcta')
+          expect(res.body).to.have.property('errores')
+
+       })
+      chai.request(app).post(`${CONTEXT_APP}/players/salaries`)
+      .send({jugadores : 'jugadores'})
+      .end( function(err,res){
+        expect(res).to.have.status(400);
+        expect(res.body).to.have.property('mensaje', 'La solicitud no cumple con una estrutura correcta')
+        expect(res.body).to.have.property('errores')
         done()
      })
    })
